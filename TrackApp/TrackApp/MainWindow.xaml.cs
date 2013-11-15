ad09b5458dd1e765950a68fb8a8375d43625bf8c
ralicatst <ralicatst@gmail.com>
2013-11-15 20:20:38 +0000

    UI Beginning

---
+++ TrackApp/TrackApp/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace TrackApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //слагам ги глобални за по-лесен достъп из класа
+        OpenFileDialog loadFileDialog;
+        string inputVideoPath;
+        string outputVideoPath;
+        public MainWindow()
+        {
+            InitializeComponent();
+            loadFileDialog = new OpenFileDialog();
+        }
+
+        private void btnLoadClick(object sender, RoutedEventArgs e)
+        {
+            loadFileDialog.Filter = "Video Files (.avi)|*.avi";
+            loadFileDialog.FilterIndex = 1;
+            loadFileDialog.Multiselect = false;
+            loadFileDialog.ShowDialog();
+            inputVideoPath = loadFileDialog.FileName;
+        }
+        private void btnAdjustSettings_Click(object sender, RoutedEventArgs e)
+        {
+            Settings windowSettings = new Settings();
+            windowSettings.Topmost = true;
+            windowSettings.Show();
+        }
+        private void btnLoadGPXClick(object sender, RoutedEventArgs e)
+        {
+            loadFileDialog.Filter = "GPX Files (.gpx)|*.gpx";
+            loadFileDialog.FilterIndex = 1;
+            loadFileDialog.Multiselect = false;
+            loadFileDialog.ShowDialog();
+            inputVideoPath = loadFileDialog.FileName;
+        }
+        #region hoverEffects
+        private void btnLoadVideo_Enter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            var imgBrush = new ImageBrush();
+            Uri uri = new Uri("Resources/iconVideo_enter.png", UriKind.Relative);
+            imgBrush.ImageSource = new BitmapImage(uri);
+            btnLoadVideo.Background = imgBrush;
+        }
+
+        private void btnLoadVideo_Leave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            var imgBrush = new ImageBrush();
+            Uri uri = new Uri("Resources/iconVideo_leave.png", UriKind.Relative);
+            imgBrush.ImageSource = new BitmapImage(uri);
+            btnLoadVideo.Background = imgBrush;
+        }
+
+        private void MainWindowLoaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnAdjustSettings_Enter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            var imgBrush = new ImageBrush();
+            Uri uri = new Uri("Resources/iconSettings_enter.png", UriKind.Relative);
+            imgBrush.ImageSource = new BitmapImage(uri);
+            btnAdjustSettings.Background = imgBrush;
+        }
+        private void btnAdjustSettings_Leave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            var imgBrush = new ImageBrush();
+            Uri uri = new Uri("Resources/iconSettings_Leave.png", UriKind.Relative);
+            imgBrush.ImageSource = new BitmapImage(uri);
+            btnAdjustSettings.Background = imgBrush;
+        }
+
+        private void btnLoadGpx_Enter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            var imgBrush = new ImageBrush();
+            Uri uri = new Uri("Resources/iconGPX_enter.png", UriKind.Relative);
+            imgBrush.ImageSource = new BitmapImage(uri);
+            btnLoadGpx.Background = imgBrush;
+        }
+        private void btnLoadGpx_Leave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            var imgBrush = new ImageBrush();
+            Uri uri = new Uri("Resources/iconGPX_leave.png", UriKind.Relative);
+            imgBrush.ImageSource = new BitmapImage(uri);
+            btnLoadGpx.Background = imgBrush;
+        }
+        #endregion
+
+        private void btnOKClick(object sender, RoutedEventArgs e)
+        {
+            //тук може да извършваме проверка дали всичко ни е въведено, след което да инициализираме класове от бекенда, ако не да пращаме message box
+        }
+    }
+        
+}
---
+++ TrackApp/TrackApp/Settings.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TrackApp
+{
+    /// <summary>
+    /// Interaction logic for Settings.xaml
+    /// </summary>
+    /// TODO:  1. Инициализираме в класа всички полета, които ще трябва да предадем надолу по бекенда(на бекенда трябва да е създаден преди това widget)
+    ///        2. Вмъкване на всички контроли
+    ///        3. ОК бутон - при него трябва да има валидация дали всички полета са попълнени (например txtMyTextBlock.Text == (null||String.Empty))
+    ///        4. След попълване на тези контроли в зависимост от резултатът им ще променим и вида на MainWindow
+    ///        
+    public partial class Settings : Window
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+    }
+}
