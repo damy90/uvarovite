5a9f249ff2eec7dbf9de9cababad3d9fa4136508
ralicatst <ralicatst@gmail.com>
2013-11-30 15:10:05 +0000

    Hover effects added, currently working on input/output buttons

--- TrackApp/TrackApp/MainWindow.xaml.cs
+++ TrackApp/TrackApp/MainWindow.xaml.cs
@@ -46,7 +46,7 @@ namespace TrackApp
             }
         }
 
-        #region Helping Methods     
+        #region Helping Methods
         private void ResizeGrid(Grid grid)
         {
             if (grid.Height == rowHeight)
@@ -236,69 +236,69 @@ namespace TrackApp
             //TODO:add validations
             //if (CheckFileGridInput())
             //{
-                //Files
-                ProjectSettings settings = ProjectSettings.GetSettings();
-                //settings.Serialize();
-                //settings.VideoEnd = 300;
-                //settings = settings.Deserialize();
-                settings.VideoOutputPath = saveFileDialog.FileName + ".avi";
-                settings.TrackStart = Convert.ToInt32(udTrackHours.Value) * 3600f + Convert.ToInt32(udTrackMinutes.Value) * 60 + Convert.ToInt32(udTrackSeconds.Value);
-                settings.VideoStart = Convert.ToInt32(udVideoStHours.Value) * 3600f + Convert.ToInt32(udVideoStMinutes.Value) * 60 + Convert.ToInt32(udVideoStSeconds.Value);
-                settings.VideoEnd = Convert.ToInt32(udVideoEndHours.Value) * 3600f + Convert.ToInt32(udVideoEndMinutes.Value) * 60 + Convert.ToInt32(udVideoEndSeconds.Value);
-                settings.BitRate = 20000000;
-                //Track
-                settings.ShowTrack = (bool)this.cbEnableTrack.IsChecked;
-                if ((bool)this.cbEnableTrack.IsChecked)
-                {
-                    settings.TrackPostion = new System.Drawing.Point(Convert.ToInt32(this.txtTrackX.Text), Convert.ToInt32(this.txtTrackY.Text));
-                    settings.TrackHeight = Convert.ToInt32(this.txtTrackHeight.Text);
-                    settings.TraveledTrackColor = System.Drawing.Color.FromArgb(this.cpTrackTravelledColor.SelectedColor.A, this.cpTrackTravelledColor.SelectedColor.R, this.cpTrackTravelledColor.SelectedColor.G, this.cpTrackTravelledColor.SelectedColor.B);
-                    settings.TraveledTrackLineWidth = Convert.ToInt32(this.txtTrackTravelledWidth.Text);
-                    settings.WholeTrackColor = System.Drawing.Color.FromArgb(this.cpTrackWholeColor.SelectedColor.A, this.cpTrackWholeColor.SelectedColor.R, this.cpTrackWholeColor.SelectedColor.G, this.cpTrackWholeColor.SelectedColor.B);
-                    settings.WholeTrackLineWidth = Convert.ToInt32(this.txtTrackWholeWidth.Text);
-                }
-                //Position marker
-                settings.ShowPositionMarker = (bool)this.cbEnablePM.IsChecked;
-                if ((bool)this.cbEnablePM.IsChecked)
-                {
-                    settings.PositionMarkerSize = Convert.ToInt32(this.txtPMSize.Text);
-                    settings.PositionMarkerColor = System.Drawing.Color.FromArgb(this.cpPMColor.SelectedColor.A, this.cpPMColor.SelectedColor.R, this.cpPMColor.SelectedColor.G, this.cpPMColor.SelectedColor.B);
-                }
-                //Overlay Image
-                settings.ShowOverlayImage = (bool)this.cbEnableOverlayImage.IsChecked;
-                if ((bool)this.cbEnableOverlayImage.IsChecked)
-                {
-                    settings.overlayImagePosition = new System.Drawing.Point(Convert.ToInt32(this.txtOIX.Text), Convert.ToInt32(this.txtOIY.Text));
-                }
-                //Elevation widget - not included in the UI so giving it the hardcoded data
-                settings.ShowElevationWidget = false;
-                settings.ElevationWidgetPosition = new System.Drawing.Point(300, 0);
-                settings.ElevationWidgetFont = new System.Drawing.Font("Arial", 28);
-                settings.ElevationWidgetColor = System.Drawing.Color.White;
-                //Map
-                settings.ShowMap = (bool)this.cbEnableMap.IsChecked;
-                if ((bool)this.cbEnableMap.IsChecked)
-                {
-                    settings.MapHeight = Convert.ToInt32(this.txtMapHeight.Text);
-                    settings.MapOpacity = (float)this.dudMapOpacity.Value;
-                }
-                //Distance
-                settings.ShowDistanceWidget = (bool)this.cbEnableDistance.IsChecked;
-                if ((bool)this.cbEnableDistance.IsChecked)
-                {
-                    settings.DistanceWidgetPosition = new System.Drawing.Point(Convert.ToInt32(this.txtDistanceX.Text), Convert.ToInt32(this.txtDistanceY.Text));
-                    settings.DistanceWidgetFont = new System.Drawing.Font(this.cbDistanceFont.SelectedValue.ToString(), Convert.ToInt32(this.txtDistanceFontSize.Text));
-                    settings.DistanceWidgetColor = System.Drawing.Color.FromArgb(this.cpDistanceColor.SelectedColor.A, this.cpDistanceColor.SelectedColor.R, this.cpDistanceColor.SelectedColor.G, this.cpDistanceColor.SelectedColor.B);
-                }
-                //Speed
-                if ((bool)this.cbEnableSpeed.IsChecked)
-                {
-                    settings.ShowSpeedWidget = (bool)this.cbEnableSpeed.IsChecked;
-                    settings.SpeedWidgetPosition = new System.Drawing.Point(Convert.ToInt32(this.txtSpeedX.Text), Convert.ToInt32(this.txtSpeedY.Text));
-                    settings.SpeedWidgetFont = new System.Drawing.Font(this.cbSpeedFont.SelectedValue.ToString(), Convert.ToInt32(this.txtSpeedFontSize.Text));
-                    settings.SpeedWidgetColor = System.Drawing.Color.FromArgb(this.cpSpeedColor.SelectedColor.A, this.cpSpeedColor.SelectedColor.R, this.cpSpeedColor.SelectedColor.G, this.cpSpeedColor.SelectedColor.B);
-                }
-                VideoCompositor.RenderVideo();
+            //Files
+            ProjectSettings settings = ProjectSettings.GetSettings();
+            //settings.Serialize();
+            //settings.VideoEnd = 300;
+            //settings = settings.Deserialize();
+            settings.VideoOutputPath = saveFileDialog.FileName + ".avi";
+            settings.TrackStart = Convert.ToInt32(udTrackHours.Value) * 3600f + Convert.ToInt32(udTrackMinutes.Value) * 60 + Convert.ToInt32(udTrackSeconds.Value);
+            settings.VideoStart = Convert.ToInt32(udVideoStHours.Value) * 3600f + Convert.ToInt32(udVideoStMinutes.Value) * 60 + Convert.ToInt32(udVideoStSeconds.Value);
+            settings.VideoEnd = Convert.ToInt32(udVideoEndHours.Value) * 3600f + Convert.ToInt32(udVideoEndMinutes.Value) * 60 + Convert.ToInt32(udVideoEndSeconds.Value);
+            settings.BitRate = 20000000;
+            //Track
+            settings.ShowTrack = (bool)this.cbEnableTrack.IsChecked;
+            if ((bool)this.cbEnableTrack.IsChecked)
+            {
+                settings.TrackPostion = new System.Drawing.Point(Convert.ToInt32(this.txtTrackX.Text), Convert.ToInt32(this.txtTrackY.Text));
+                settings.TrackHeight = Convert.ToInt32(this.txtTrackHeight.Text);
+                settings.TraveledTrackColor = System.Drawing.Color.FromArgb(this.cpTrackTravelledColor.SelectedColor.A, this.cpTrackTravelledColor.SelectedColor.R, this.cpTrackTravelledColor.SelectedColor.G, this.cpTrackTravelledColor.SelectedColor.B);
+                settings.TraveledTrackLineWidth = Convert.ToInt32(this.txtTrackTravelledWidth.Text);
+                settings.WholeTrackColor = System.Drawing.Color.FromArgb(this.cpTrackWholeColor.SelectedColor.A, this.cpTrackWholeColor.SelectedColor.R, this.cpTrackWholeColor.SelectedColor.G, this.cpTrackWholeColor.SelectedColor.B);
+                settings.WholeTrackLineWidth = Convert.ToInt32(this.txtTrackWholeWidth.Text);
+            }
+            //Position marker
+            settings.ShowPositionMarker = (bool)this.cbEnablePM.IsChecked;
+            if ((bool)this.cbEnablePM.IsChecked)
+            {
+                settings.PositionMarkerSize = Convert.ToInt32(this.txtPMSize.Text);
+                settings.PositionMarkerColor = System.Drawing.Color.FromArgb(this.cpPMColor.SelectedColor.A, this.cpPMColor.SelectedColor.R, this.cpPMColor.SelectedColor.G, this.cpPMColor.SelectedColor.B);
+            }
+            //Overlay Image
+            settings.ShowOverlayImage = (bool)this.cbEnableOverlayImage.IsChecked;
+            if ((bool)this.cbEnableOverlayImage.IsChecked)
+            {
+                settings.overlayImagePosition = new System.Drawing.Point(Convert.ToInt32(this.txtOIX.Text), Convert.ToInt32(this.txtOIY.Text));
+            }
+            //Elevation widget - not included in the UI so giving it the hardcoded data
+            //settings.ShowElevationWidget = false;
+            settings.ElevationWidgetPosition = new System.Drawing.Point(300, 0);
+            settings.ElevationWidgetFont = new System.Drawing.Font("Arial", 28);
+            settings.ElevationWidgetColor = System.Drawing.Color.White;
+            //Map
+            settings.ShowMap = (bool)this.cbEnableMap.IsChecked;
+            if ((bool)this.cbEnableMap.IsChecked)
+            {
+                settings.MapHeight = Convert.ToInt32(this.txtMapHeight.Text);
+                settings.MapOpacity = (float)this.dudMapOpacity.Value;
+            }
+            //Distance
+            settings.ShowDistanceWidget = (bool)this.cbEnableDistance.IsChecked;
+            if ((bool)this.cbEnableDistance.IsChecked)
+            {
+                settings.DistanceWidgetPosition = new System.Drawing.Point(Convert.ToInt32(this.txtDistanceX.Text), Convert.ToInt32(this.txtDistanceY.Text));
+                settings.DistanceWidgetFont = new System.Drawing.Font(this.cbDistanceFont.SelectedValue.ToString(), Convert.ToInt32(this.txtDistanceFontSize.Text));
+                settings.DistanceWidgetColor = System.Drawing.Color.FromArgb(this.cpDistanceColor.SelectedColor.A, this.cpDistanceColor.SelectedColor.R, this.cpDistanceColor.SelectedColor.G, this.cpDistanceColor.SelectedColor.B);
+            }
+            //Speed
+            if ((bool)this.cbEnableSpeed.IsChecked)
+            {
+                settings.ShowSpeedWidget = (bool)this.cbEnableSpeed.IsChecked;
+                settings.SpeedWidgetPosition = new System.Drawing.Point(Convert.ToInt32(this.txtSpeedX.Text), Convert.ToInt32(this.txtSpeedY.Text));
+                settings.SpeedWidgetFont = new System.Drawing.Font(this.cbSpeedFont.SelectedValue.ToString(), Convert.ToInt32(this.txtSpeedFontSize.Text));
+                settings.SpeedWidgetColor = System.Drawing.Color.FromArgb(this.cpSpeedColor.SelectedColor.A, this.cpSpeedColor.SelectedColor.R, this.cpSpeedColor.SelectedColor.G, this.cpSpeedColor.SelectedColor.B);
+            }
+            VideoCompositor.RenderVideo();
             //}
             //else
             //{
@@ -309,64 +309,67 @@ namespace TrackApp
         #region HoverEffects
         public void btnWidgets_MouseEnter(object sender, MouseEventArgs e)
         {
+            ChangeIconBackground(@"pack://application:,,,/Resources/SB_Widgets_black.png", btnWidgets);
         }
         public void btnWidgets_MouseLeave(object sender, MouseEventArgs e)
         {
+            ChangeIconBackground(@"pack://application:,,,/Resources/SB_Widgets_white.png", btnWidgets);
         }
         public void btnFiles_MouseEnter(object sender, MouseEventArgs e)
         {
+            ChangeIconBackground(@"pack://application:,,,/Resources/SB_Files_black.png", btnFiles);
         }
         public void btnFiles_MouseLeave(object sender, MouseEventArgs e)
         {
+            ChangeIconBackground(@"pack://application:,,,/Resources/SB_Files_white.png", btnFiles);
         }
         private void btnTrack_MouseEnter(object sender, MouseEventArgs e)
         {
-
+            ChangeIconBackground(@"pack://application:,,,/Resources/SB_Track_black.png", btnTrack);
         }
         private void btnTrack_MouseLeave(object sender, MouseEventArgs e)
         {
-
+            ChangeIconBackground(@"pack://application:,,,/Resources/SB_Track_white.png", btnTrack);
         }
         private void btnSpeed_MouseEnter(object sender, MouseEventArgs e)
         {
-
+            ChangeIconBackground(@"pack://application:,,,/Resources/SB_Speed_black.png", btnSpeed);
         }
-
         private void btnSpeed_MouseLeave(object sender, MouseEventArgs e)
         {
-
+            ChangeIconBackground(@"pack://application:,,,/Resources/SB_Speed_white.png", btnSpeed);
         }
         private void btnDistance_MouseEnter(object sender, MouseEventArgs e)
         {
-
+            ChangeIconBackground(@"pack://application:,,,/Resources/SB_Distance_black.png", btnDistance);
         }
         private void btnDistance_MouseLeave(object sender, MouseEventArgs e)
         {
-
+            ChangeIconBackground(@"pack://application:,,,/Resources/SB_Distance_white.png", btnDistance);
         }
         private void btnPositionMarker_MouseEnter(object sender, MouseEventArgs e)
         {
-
+            ChangeIconBackground(@"pack://application:,,,/Resources/SB_Position-Marker_black.png", btnPositionMarker);
         }
         private void btnPositionMarker_MouseLeave(object sender, MouseEventArgs e)
         {
-
+            ChangeIconBackground(@"pack://application:,,,/Resources/SB_Position-Marker_white.png", btnPositionMarker);
         }
         private void btnOverlayImage_MouseEnter(object sender, MouseEventArgs e)
         {
-
+            ChangeIconBackground(@"pack://application:,,,/Resources/SB_Overlay_Image_black.png", btnOverlayImage);
         }
         private void btnOverlayImage_MouseLeave(object sender, MouseEventArgs e)
         {
-
+            ChangeIconBackground(@"pack://application:,,,/Resources/SB_Overlay_Image_white.png", btnOverlayImage);
         }
         private void btnMap_MouseEnter(object sender, MouseEventArgs e)
         {
-
+            ChangeIconBackground(@"pack://application:,,,/Resources/SB_Map_black.png", btnMap);
         }
         private void btnMap_MouseLeave(object sender, MouseEventArgs e)
         {
-
+            ChangeIconBackground(@"pack://application:,,,/Resources/SB_Map_white.png", btnMap);
         }
         private void btnLoadVideo_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
         {
@@ -393,6 +396,7 @@ namespace TrackApp
             ChangeIconBackground(@"pack://application:,,,/Resources/SB_Syncronization_leave.png", btnSync);
         }
         #endregion
+        #region ComboEffects
         private void cmbEncoding_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             VideoFormats format = (VideoFormats)cmbEncoding.SelectedIndex;
@@ -400,7 +404,7 @@ namespace TrackApp
         }
 
         private void cbEnableTrack_Checked(object sender, RoutedEventArgs e)
-        {     
+        {
             this.grdEnabledTrack.Visibility = Visibility.Visible;
         }
 
@@ -476,4 +480,5 @@ namespace TrackApp
             saveFileDialog.ShowDialog();
         }
     }
+        #endregion
 }
